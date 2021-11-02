c53838ef83ffb8c9b11d6fe6982bb93a8533422b
Franco Antonio Paolo <franctop360@gmail.com>
2021-11-01 22:50:01 -0300

    formulario

---
+++ Modulo 3/Unidad 3/routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require("nodemailer");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var telefono = req.body.telefono;
+
+  //console.log(req.body);
+
+
+  var obj = {
+    to: 'franctop360@gmail.com',
+    subject: 'Contacto desde formulario',
+    html: nombre + " "+ apellido + " se contacto a traves del formulario y quiere mas informacion en este correo: " + email + " " + "o al siguiente telefono: " + telefono
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+
+  });
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Enviado correctamente',
+  });
+
+
+
+});
+
+
+
+module.exports = router;
+
